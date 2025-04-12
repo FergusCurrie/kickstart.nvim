d7b2e18b659fb30418be4b7b9291c9899e6df103
FergusCurrie <fergus.currie@gmail.com>
2025-04-13 08:54:56 +1000

    flash, autopairs

--- init.lua
+++ init.lua
@@ -278,7 +278,13 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
   {
     'rose-pine/neovim',
     {},
@@ -286,6 +292,21 @@ require('lazy').setup({
   {
     'Mofiqul/vscode.nvim',
     {},
+  },
+  { 'norcalli/nvim-colorizer.lua' },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
   },
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
