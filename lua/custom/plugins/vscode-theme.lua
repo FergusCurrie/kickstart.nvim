0442fe0271fde7a33c1efcc6a9646bd3dc008f5c
FergusCurrie <fergus.currie@gmail.com>
2025-04-15 20:21:41 +1000

    modularised plugins

---
+++ lua/custom/plugins/vscode-theme.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'Mofiqul/vscode.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+
+      vim.cmd.colorscheme 'vscode'
+    end,
+  },
+}
